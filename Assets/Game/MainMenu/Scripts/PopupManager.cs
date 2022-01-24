26ca34c2fbf1278daf96f8cdcb98efc647deabc4
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2022-01-24 22:33:53 +0900

    [추가] 팝업 버튼 기능 동적 전달

--- Assets/Game/MainMenu/Scripts/PopupManager.cs
+++ Assets/Game/MainMenu/Scripts/PopupManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PopupManager : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class PopupManager : MonoBehaviour
     public Text textTitle;
     public Text textDesc;
     public Button btnConfirm;
+    public string curMethod;
+    public string curParameter;
     void Start()
     {
         textTitle.GetComponent<Text>().text = "";
@@ -28,8 +31,27 @@ public class PopupManager : MonoBehaviour
     public void setDesc(string desc) {
         textDesc.GetComponent<Text>().text = desc;
     }
-    
-    public void handleOnClick(UnityEngine.Events.UnityAction onClick) {
-        btnConfirm.GetComponent<Button>().onClick.AddListener(onClick);
+    public void setMethod(string methodKey) {
+        curMethod = methodKey;
+        curParameter = "";
+    }
+    public void setParameter(string methodParameter) {
+        curParameter = methodParameter;
+    }
+    public void handleOnClick() {
+        switch (curMethod) {
+            case "startNew":
+                UIManager.handleStartNew();
+                break;
+            case "startLoad":
+                int temp = int.Parse(curParameter);
+                UIManager.handleStartLoad(temp);
+                break;
+            case "terminate":
+                UIManager.terminate();
+                break;
+            default:
+                break;
+        }
     }
 }
